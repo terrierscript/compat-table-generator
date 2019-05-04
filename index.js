bde89c66ae6c4625b7fe4b00ec911b4a1dd61d2d
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-05-04 22:02:44 +0900

    add

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const bcd = require("mdn-browser-compat-data")
-const target = bcd.javascript.builtins.Array.flat
+// const table = require("markdown-table")
 
-const head = (name) => {
-  return `![${name}][${name.toLowerCase()} - image]`
-}
-const browsers = ["Chrome", "Edge", "Firefox", "IE", "Opera", "Safari"]
-
-browsers.map((name) => {
-  console.log(target.__compat.support[name.toLowerCase()])
-  return {
-    name: 
+const generate = (category, fnName) => {
+  const target = bcd.javascript.builtins[category][fnName]
+  const browserIcon = (name) => {
+    return `![${name}][${name.toLowerCase()}-image]`
   }
-})
+  const browsers = ["Chrome", "Edge", "Firefox", "IE", "Opera", "Safari"]
+
+  const toTable = (versions) => {
+    return versions
+      .reduce(
+        (curr, { name, version }, i) => {
+          curr[0][i] = browserIcon(name)
+          curr[1][i] = ":-:"
+          curr[2][i] = version
+          return curr
+        },
+        [[], [], []]
+      )
+      .map((row) => {
+        return row.join(" | ") + "　|"
+      })
+      .join("\n")
+  }
+
+  const supportTable = browsers.map((name) => {
+    const support = target.__compat.support[name.toLowerCase()]
+    const version = support.version_added ? `${support.version_added} ✔` : "✖"
+
+    return {
+      name,
+      version
+    }
+  })
+  const head = `#### Browser Support for \`${category}.prototype.${fnName}\`\n`
+  console.log(head)
+  console.log(toTable(supportTable))
+}
+
+generate("Array", "flat")
